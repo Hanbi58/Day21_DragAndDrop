9dd1d578627e9f9f96b7d26d59fe1b307404daa4
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-19 14:07:00 -0700

    Teacher's version done and bug fixed

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-alert('1')
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  this.className += " hold";
+
+  setTimeout(() => (this.className = "jimo"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.style.backgroundColor = "#333";
+}
+
+function dragLeave() {
+  this.className = "empty";
+  this.style.backgroundColor = "white";
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
